56a633dce742803e16fdcd2e498b6affffb5ac26
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-09-30 15:19:18 +0300

    отобразить строку

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -8,5 +8,49 @@ import { Circle } from "../ui/circle/circle";
 import { ElementStates } from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
-  return <SolutionLayout title="Строка"></SolutionLayout>;
+  const input = document.getElementById("string-input");
+  const [stringArr, setStringArr] = useState([null]);
+
+  const [disableBtn, setDisableBtn] = useState(true);
+  const handleInput = () => {
+    setDisableBtn(false);
+  };
+  const handleClick = () => {
+    const string = input?.value;
+    const arr = string.split("");
+    setStringArr(arr);
+    bubbleSort(arr);
+  };
+
+  const bubbleSort = (arr: number[]): number[] => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  };
+
+  return (
+    <SolutionLayout title="Строка">
+      <div className={styles.input__container}>
+        <Input
+          onInput={(evt) => handleInput(evt)}
+          maxLength={11}
+          type={"text"}
+          isLimitText={true}
+          extraClass={styles.string_input}
+          id="string-input"
+        />
+        <Button onClick={handleClick} text="Развернуть" disabled={disableBtn} />
+      </div>
+      <div className={styles.letters__container}>
+        {stringArr.length > 1 &&
+          stringArr.map((stringEl, i) => {
+            return <Circle state={ElementStates.Changing} key={i} letter={stringEl} />;
+          })}
+      </div>
+    </SolutionLayout>
+  );
 };
