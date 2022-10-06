86819f54a6dc0b088c1bd2f169a5d4501a6fc4fe
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-10-06 16:05:06 +0300

    Update sorting-page.tsx

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -10,53 +10,70 @@ import styles from "./sorting-page.module.css";
 
 export const SortingPage: React.FC = () => {
   const [data, setData] = useState([1, 5, 54, 30, 10, 3]);
+  let time = 1000;
 
-  const bubbleSortAsc = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[i] < arr[j]) {
-          [arr[i], arr[j]] = [arr[j], arr[i]];
-          setData([...arr]);
-        }
-      }
-    }
-  };
+  // const bubbleSortAsc = (arr) => {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     for (let j = 0; j < arr.length; j++) {
+  //       if (arr[i] < arr[j]) {
+  //         time += 1000;
+  //         setTimeout(() => {
+  //           [arr[i], arr[j]] = [arr[j], arr[i]];
+  //           setData([...arr]);
+  //         }, time);
+  //       }
+  //     }
+  //   }
+  // };
 
-  const bubbleSortDesc = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-          [arr[i], arr[j]] = [arr[j], arr[i]];
-          setData([...arr]);
-        }
-      }
-    }
-  };
+  // const bubbleSortDesc = (arr) => {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     for (let j = 0; j < arr.length; j++) {
+  //       if (arr[i] > arr[j]) {
+  //         time += 1000;
+  //         setTimeout(() => {
+  //           [arr[i], arr[j]] = [arr[j], arr[i]];
+  //           setData([...arr]);
+  //         }, time);
+  //       }
+  //     }
+  //   }
+  // };
 
   const selectionSortAsc = (arr: number[]): number[] => {
     for (let i = 0; i < arr.length - 1; i++) {
       let minIdx = i;
       for (let j = i + 1; j < arr.length; j++) {
         if (arr[minIdx] > arr[j]) {
-          minIdx = j;
+          time += 1000;
+          setTimeout(() => {
+            minIdx = j;
+            [arr[minIdx], arr[i]] = [arr[i], arr[minIdx]];
+            setData([...arr]);
+          }, time);
         }
       }
-      [arr[minIdx], arr[i]] = [arr[i], arr[minIdx]];
-      setData([...arr]);
     }
     return arr;
   };
 
   const selectionSortDesc = (arr: number[]): number[] => {
     for (let i = 0; i < arr.length - 1; i++) {
-      let maxIdx = i;
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[maxIdx] < arr[j]) {
-          maxIdx = j;
+      time += 500;
+      selectionSort(arr, i);
+    }
+
+    function selectionSort(arr, i) {
+      setTimeout(() => {
+        let maxIdx = i;
+        for (let j = i + 1; j < arr.length; j++) {
+          if (arr[maxIdx] < arr[j]) {
+            maxIdx = j;
+          }
         }
-      }
-      [arr[maxIdx], arr[i]] = [arr[i], arr[maxIdx]];
-      setData([...arr]);
+        [arr[maxIdx], arr[i]] = [arr[i], arr[maxIdx]];
+        setData([...arr]);
+      }, time);
     }
     return arr;
   };
