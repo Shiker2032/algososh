5f4a3bdaba84ae13b54d161b6ab3d694ffad4106
Vladislav <53581923+Shiker2032@users.noreply.github.com>
2022-10-28 16:43:03 +0300

    Тестирование circle и button

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import { Button } from "./button"
+
+describe("Button tests", () => {
+  test("button with text", () => {
+    render(<Button text="test" />)
+    expect(screen.getByRole("button")).toHaveTextContent("test")
+  })
+
+  test("button no text", () => {
+    render(<Button />)
+    expect(screen.getByRole("button")).not.toHaveTextContent("test")
+  })
+
+  test("button enabled", () => {
+    render(<Button />)
+    expect(screen.getByRole("button")).toBeEnabled()
+  })
+
+  test("button disabled", () => {
+    render(<Button disabled={true} />)
+    expect(screen.getByRole("button")).not.toBeEnabled()
+  })
+
+  test("button loader", () => {
+    render(<Button isLoader={true} />)
+    expect(screen.getByRole("button")).toHaveClass("loader")
+    expect(screen.getByRole("img")).toBeInTheDocument()
+  })
+
+  test("button callback on click", () => {
+    const mockFunc = jest.fn()
+    render(<Button onClick={mockFunc} />)
+    fireEvent.click(screen.getByRole("button"))
+    expect(mockFunc).toBeCalledTimes(1)
+  })
+})
--- src/components/ui/circle/circle.tsx
+++ src/components/ui/circle/circle.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styles from "./circle.module.css";
-import { ElementStates } from "../../../types/element-states";
+import React from "react"
+import styles from "./circle.module.css"
+import { ElementStates } from "../../../types/element-states"
 
 interface CircleProps {
-  state?: ElementStates;
-  letter?: string | number;
-  head?: string | React.ReactElement | null;
-  index?: number;
-  tail?: string | React.ReactElement | null;
-  tailType?: "string" | "element";
-  extraClass?: string;
-  isSmall?: boolean;
+  state?: ElementStates
+  letter?: string | number
+  head?: string | React.ReactElement | null
+  index?: number
+  tail?: string | React.ReactElement | null
+  tailType?: "string" | "element"
+  extraClass?: string
+  isSmall?: boolean
 }
 
 export const Circle: React.FC<CircleProps> = ({
@@ -25,24 +25,33 @@ export const Circle: React.FC<CircleProps> = ({
   return (
     <div className={`${styles.content} ${extraClass}`}>
       <div
+        data-testid="head div"
         className={`text text_type_input text_color_input mb-4 ${styles.absolute} ${styles.head} ${
           styles[typeof head === "string" ? "string" : "element"]
         }`}>
         {head}
       </div>
-      <div className={`${styles.circle}  ${isSmall ? styles.small : ""} ${styles[state]}`}>
-        <p className={`text text_type_circle text_color_input ${styles.letter}`}>{letter}</p>
+      <div
+        data-testid="circle div"
+        className={`${styles.circle}  ${isSmall ? styles.small : ""} ${styles[state]}`}>
+        <p
+          data-testid="letter p"
+          className={`text text_type_circle text_color_input ${styles.letter}`}>
+          {letter}
+        </p>
       </div>
       <p
+        data-testid="index p"
         className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${styles.index}`}>
         {index?.toString()}
       </p>
       <div
+        data-testid="tail div"
         className={`text text_type_input text_color_input mt-4 ${styles.absolute} ${
           index?.toString() ? styles.tail60 : styles.tail30
         } ${styles[typeof tail === "string" ? "string" : "element"]}`}>
         {tail}
       </div>
     </div>
-  );
-};
+  )
+}
